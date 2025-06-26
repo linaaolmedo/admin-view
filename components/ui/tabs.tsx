aa0d029b422a7ffc6af79fce8833c3c11765f229
LinaOlmedo <linaaolmedo@gmail.com>
2025-06-26 12:44:09 -0700

    final test pt.2

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
 
-import { cn } from "@/lib/utils" // comment to test the correct repo
+import { cn } from "@/lib/utils" // comment to test the correct repo pt.2
 
 const Tabs = TabsPrimitive.Root
 
